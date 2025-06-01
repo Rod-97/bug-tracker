ec17ffec81c4b3a123ef212725e2b6c81305825b
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-06-01 13:58:03 +0200

    seed function

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,31 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { usersTable } from "./schema";
+import { seed } from "drizzle-seed";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
-  const user1: typeof usersTable.$inferInsert = {
-    name: "Ronaldo",
-    email: "ronaldo@example.com",
-  };
-
-  const user2: typeof usersTable.$inferInsert = {
-    name: "Messi",
-    email: "messi@example.com",
-  };
-
-  const user3: typeof usersTable.$inferInsert = {
-    name: "Neymar",
-    email: "neymar@example.com",
-  };
-
-  const users = [user1, user2, user3];
-
-  users.forEach(async (user) => {
-    await db.insert(usersTable).values(user);
-    console.log(`New user created: ${user.name}`);
-  });
+  await seed(db, { usersTable });
 }
 
 main();
