b789355f519f8d66efdf7bc2913a5cdbc484c8b9
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-06-02 23:37:44 +0200

    session table

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,8 +1,26 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("users", {
-  id: uuid().defaultRandom().primaryKey(),
+  id: serial("id").primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
   name: varchar({ length: 255 }).notNull(),
   passwordHash: varchar().notNull(),
+});
+
+export const sessionTable = pgTable("sessions", {
+  id: text("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
 });
